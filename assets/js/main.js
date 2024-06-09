0792d3f73ea31f44b9ebf5691f3af60f93602cd1
Duygu Jones <duygujones@gmail.com>
2024-06-10 02:03:20 +0300

    licence

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Landed by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 //--Logo Scroll up settings---------------------
 document.getElementById('scrollToTopLogo').addEventListener('click', function() {
